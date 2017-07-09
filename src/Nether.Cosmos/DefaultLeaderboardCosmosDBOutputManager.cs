b5895038e2cd7043ad9eb269a3d7f47cb827c1d0
Nava Vaisman Levy <navale@microsoft.com>
2017-07-09 13:57:03 +0300

    initial leaderboard implementation

---
+++ src/Nether.Cosmos/DefaultLeaderboardCosmosDBOutputManager.cs
@@ -0,0 +1,28 @@
+﻿using Nether.Ingest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nether.Cosmos
+{
+    public class DefaultLeaderboardCosmosDBOutputManager : IOutputManager
+    {
+        private CsvMessageFormatter scoreSerializer;
+
+        public DefaultLeaderboardCosmosDBOutputManager(CsvMessageFormatter scoreSerializer)
+        {
+            this.scoreSerializer = scoreSerializer;
+        }
+
+        public Task FlushAsync(string partitionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OutputMessageAsync(string partitionId, string pipelineName, int index, Message msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
